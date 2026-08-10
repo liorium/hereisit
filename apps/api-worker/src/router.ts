0c7fad4084b6479e461c9b9c5a3a7bb291bddfe6
liorium <madeby.liora@gmail.com>
2026-08-10 19:38:31 +0000

    fix: expose processing rate-limit scope

--- apps/api-worker/src/router.ts
+++ apps/api-worker/src/router.ts
@@ -31,7 +31,8 @@ import { routeUploadRequest, type UploadRouteRuntime } from "./routes/uploads";
 
 const ALLOW_METHODS = "GET, POST, PUT, DELETE, OPTIONS";
 const ALLOW_HEADERS = "authorization, content-type, x-download-lease";
-const EXPOSE_HEADERS = "content-length, content-type, etag, retry-after, x-download-lease";
+const EXPOSE_HEADERS =
+  "content-length, content-type, etag, retry-after, x-download-lease, x-hereisit-rate-limit-scope";
 const UPLOAD_PATH_PATTERN =
   /^\/v1\/jobs\/([0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})\/input$/;
 const JOB_PATH_PATTERN =
